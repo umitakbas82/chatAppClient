41e5013024618490b9fb9706688ccb0845670d13
umitakbas82 <umit.akbas@hotmail.com.tr>
2025-06-18 10:43:57 +0300

    register component added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+name:string=""
+
+constructor(private http:HttpClient, private router:Router){}
+
+
+login(){
+  this.http.get("https://localhost:7144/api/Auth/Login?name="+this.name).subscribe(resp=>{
+    localStorage.setItem("accesstoken",JSON.stringify(resp));
+    this.router.navigateByUrl("/")
+  })
+}
 
 }
