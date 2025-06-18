438407409c23853371261fb46d89155f5a3b5c17
umitakbas82 <umit.akbas@hotmail.com.tr>
2025-06-18 10:48:03 +0300

    komm mit

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,11 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+register:RegisterModelDTO= new RegisterModelDTo
+
+constructor(private http:HttpClient, private router:Router){}
+
+login(){
+  this.http.get("https://localhost:7144/api/Auth/Login?name="+this.name).subscribe(resp=>{
+    localStorage.setItem("accesstoken",JSON.stringify(resp));
+    this.router.navigateByUrl("/")
+  })
+}
+
 
 }
