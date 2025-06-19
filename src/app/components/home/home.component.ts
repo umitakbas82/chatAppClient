fb4034da39dbe3860b31900c20d4060f15a7813a
umitakbas82 <umit.akbas@hotmail.com.tr>
2025-06-19 12:18:54 +0300

    commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserModelDTO } from '../../models/userModelDTO';
+import { chatModelDTO } from '../../models/chatModelDTO';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +10,35 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+users:UserModelDTO[]=[];
+chats:chatModelDTO[]=[];
+selectedUserID:string="";
+selectedUser:UserModelDTO= new UserModelDTO();
 
+user=new UserModelDTO();
 
+constructor(private http:HttpClient){}
   
+logout(){
+  localStorage.clear();
+  document.location.reload()
 }
+
+getUsers(){}
+
+
+
+changeUser(user:UserModelDTO){
+  this.selectedUserID=user.id;
+  this.selectedUser=user;
+
+  this.http.get(`https://localhost:7144/api/Chats/GetChats?userId=${this.user.id}&toUserId=${this.selectedUserID}`).subscribe((res:any)=>{
+    this.chats = res;
+  });
+}
+
+
+}
+
+
+
